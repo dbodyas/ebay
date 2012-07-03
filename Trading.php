c0ac62ff297fd86ecf554f78fe15efa8a6325c09
Rick <rick@earleyholdings.com>
2012-07-03 18:28:37 -0400

    Created class stubs for the trading and shopping API

--- Trading.php
+++ Trading.php
@@ -1,7 +1,12 @@
-<?php
+<?php namespace rearley\Ebay;
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Ebay Trading API
+ * @author Rick Earley <rick@earleyholdings.com>
+ * @category Ebay API
+ * @package Trading API
+ * @version 1.0 
  */
-?>
+class Trading {
+    
+}
