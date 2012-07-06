e6f4bcc68f137075ac66bc6528f14ecb74bfea9f
Rick <rick@earleyholdings.com>
2012-07-06 14:40:58 -0400

    Added AddDisputeResponse Call Reference - Trading API

--- Trading.php
+++ Trading.php
@@ -42,8 +42,7 @@ class Trading {
     /**
      * Input Fields 
      */
-    private $standard_input_fields = array();
-    private $call_input_fields = array();    
+    private $input_fields = array();   
     
     
     /**
@@ -82,6 +81,8 @@ class Trading {
      */
     public function AddDispute(){
         
+        $this->call_reference = 'AddDispute';
+        
         // Start Request
         $request = '<AddDisputeRequest xmlns="urn:ebay:apis:eBLBaseComponents">';
         
@@ -114,14 +115,14 @@ class Trading {
         /**
          * Either ItemID/TransactionID has to be set or OrderLineItemID 
          */
-        if(isset($this->standard_input_fields['OrderLineItemID'])){
+        if(isset($this->input_fields['OrderLineItemID'])){
             
             $request .= trim($this->_OrderLineItemID());
             
         } else {
             
             // itemID and TransactionID BOTH have to be set
-            if(isset($this->standard_input_fields['ItemID']) && isset($this->standard_input_fields['TransactionID'])){
+            if(isset($this->input_fields['ItemID']) && isset($this->input_fields['TransactionID'])){
                 
                 // ItemID
                 $request .= trim($this->_ItemID());
@@ -141,8 +142,100 @@ class Trading {
         // End Request
         $request .= '</AddDisputeRequest>';
         
-        echo $request;
+        return $this->_send($request);      
+               
+    }
+    
+    /**
+     * AddDisputeResponse Call Reference
+     * @access public
+     * @link http://developer.ebay.com/DevZone/XML/docs/Reference/eBay/AddDisputeResponse.html
+     * @return boolean 
+     */
+    public function AddDisputeResponse(){
         
+        $this->call_reference = 'AddDisputeResponse';
+                
+        // Open
+        $request = '<AddDisputeResponseRequest xmlns="urn:ebay:apis:eBLBaseComponents">';
+        
+        // RequestCredentials
+        $request .= $this->_build_request_credentials();
+        
+        // ErrorLanguage
+        $request .= trim($this->_ErrorLanguage());
+        
+        // MessageID
+        $request .= trim($this->_MessageID());
+        
+        // WarningLevel
+        $request .= trim($this->_WarningLevel());
+        
+        // DisputeActivity
+        $request_required = trim($this->_required('DisputeActivity', $this->_DisputeActivity()));
+        if($request_required == FALSE){
+            return FALSE;
+        }
+        $request .= $request_required;
+        
+        // DisputeID
+        $request_required = trim($this->_required('DisputeID', $this->_DisputeID()));
+        if($request_required == FALSE){
+            return FALSE;
+        }
+        $request .= $request_required;
+        
+        /**
+         * MessageText
+         * Only required for certain activities 
+         */
+        if(isset($this->input_fields['DisputeActivity'])){
+            
+            // Required Activities
+            $required_activities = array('SellerAddInformation','SellerComment','SellerPaymentNotReceived');
+            
+            if(in_array($this->input_fields['DisputeActivity'], $required_activities)){
+                
+                $request_required = trim($this->_required('MessageText', $this->_MessageText()));
+                if($request_required == FALSE){
+                    return FALSE;
+                }
+                $request .= $request_required;
+            }
+        }
+        
+        /*
+         * ShipmentTrackNumber and ShippingCarrierUsed are required
+         * if activity is SellerShippedItem
+         */
+        if(isset($this->input_fields['DisputeActivity']) && $this->input_fields['DisputeActivity'] == 'SellerShippedItem'){
+            
+            // ShipmentTrackNumber
+            $request_required = trim($this->_required('ShipmentTrackNumber', $this->_ShipmentTrackNumber()));
+            if($request_required == FALSE){
+                return FALSE;
+            }
+            $request .= $request_required;
+            
+            // ShippingCarrierUsed
+            $request_required = trim($this->_required('ShippingCarrierUsed', $this->_ShippingCarrierUsed()));
+            if($request_required == FALSE){
+                return FALSE;
+            }
+            $request .= $request_required;
+            
+            // ShippingTime
+            $request_required = trim($this->_required('ShippingTime', $this->_ShippingTime()));
+            if($request_required == FALSE){
+                return FALSE;
+            }
+            $request .= $request_required;
+        }
+        
+        // Close
+        $request .= '</AddDisputeResponseRequest>';
+
+        return $this->_send($request);
     }
     
     
@@ -158,13 +251,49 @@ class Trading {
      */
     
     /**
+     * Set user token used on most trading calls
+     * @access public
+     * @param string $token 
+     */
+    public function UserToken($token){
+        $this->user_token = $token;
+    }
+    
+    /**
+     * Set DevID
+     * @access public
+     * @param string $devid 
+     */
+    public function DevID($devid){
+        $this->developer_id = trim($devid);
+    }
+    
+    /**
+     * Set AppID
+     * @access public
+     * @param string $appid 
+     */
+    public function AppID($appid){
+        $this->application_id = trim($appid);
+    }
+    
+    /**
+     * Set CertID
+     * @access public
+     * @param string $certid 
+     */
+    public function CertID($certid){
+        $this->certificate_id = trim($certid);
+    }
+    
+    /**
      * ErrorLanugage - Standard Input Field
      * @access public
      * @param string $error_language 
      */
     public function ErrorLanguage($error_language){
         
-        $this->standard_input_fields['ErrorLanguage'] = trim($error_language);
+        $this->input_fields['ErrorLanguage'] = trim($error_language);
         
     }
     
@@ -175,7 +304,7 @@ class Trading {
      */
     public function MessageID($message_id){
         
-        $this->standard_input_fields['MessageID'] = trim($message_id);
+        $this->input_fields['MessageID'] = trim($message_id);
     }
     
     /**
@@ -186,13 +315,13 @@ class Trading {
     public function WarningLevel($warning_level){
         
         // standardize
-        $warning_level = strtolower(trim($warning_level));
+        $warning_level = ucfirst(strtolower(trim($warning_level)));
         
         // Can either be high or low. Default low for invaild input
-        if($warning_level !== 'low' and $warning_level !== 'high'){
-            $this->standard_input_fields['WarningLevel'] = 'low';
+        if($warning_level !== 'Low' and $warning_level !== 'High'){
+            $this->input_fields['WarningLevel'] = 'Low';
         } else {
-            $this->standard_input_fields['WarningLevel'] = strtolower(trim($warning_level));
+            $this->input_fields['WarningLevel'] = $warning_level;
         }
         
     }
@@ -204,7 +333,7 @@ class Trading {
      */
     public function DisputeExplanation($dispute_explanation){
         
-        $this->standard_input_fields['DisputeExplanation'] = trim($dispute_explanation);        
+        $this->input_fields['DisputeExplanation'] = trim($dispute_explanation);        
     }
     
     /**
@@ -213,7 +342,7 @@ class Trading {
      * @param string $dispute_reason 
      */
     public function DisputeReason($dispute_reason){
-        $this->standard_input_fields['DisputeReason'] = trim($dispute_reason);
+        $this->input_fields['DisputeReason'] = trim($dispute_reason);
     }
     
     /**
@@ -223,7 +352,7 @@ class Trading {
      */
     public function ItemID($item_id){
         
-        $this->standard_input_fields['ItemID'] = trim($item_id);
+        $this->input_fields['ItemID'] = trim($item_id);
     }
     
     /**
@@ -233,7 +362,7 @@ class Trading {
      */
     public function OrderLineItemID($OrderLineItemID){
         
-        $this->standard_input_fields['OrderLineItemID'] = trim($OrderLineItemID);
+        $this->input_fields['OrderLineItemID'] = trim($OrderLineItemID);
     }
     
     /**
@@ -243,8 +372,65 @@ class Trading {
      */
     public function TransactionID($TransactionID){
         
-        $this->standard_input_fields['TransactionID'] = trim($TransactionID);
+        $this->input_fields['TransactionID'] = trim($TransactionID);
     }
+    
+    /**
+     * DisputeActivity - Call Input Field
+     * @access public
+     * @param string $DisputeActivity 
+     */
+    public function DisputeActivity($DisputeActivity){
+        
+        $this->input_fields['DisputeActivity'] = trim($DisputeActivity);
+    }
+    
+    /**
+     * DisputeID - Call Input Field
+     * @access public
+     * @param string $DisputeID 
+     */
+    public function DisputeID($DisputeID){
+        
+        $this->input_fields['DisputeID'] = trim($DisputeID);
+    }
+    
+    /**
+     * MessageText - Call Input Field
+     * @access public
+     * @param string $MessageText 
+     */
+    public function MessageText($MessageText){
+        $this->input_fields['MessageText'] = trim($MessageText);
+    }
+    
+    /**
+     * ShipmentTrackingNumber - Call Input Field
+     * @access public
+     * @param string $ShipmentTrackNumber 
+     */
+    public function ShipmentTrackNumber($ShipmentTrackNumber){
+        $this->input_fields['ShipmentTrackNumber'] = trim($ShipmentTrackNumber);
+    }
+    
+    /**
+     * ShippingCarrierUsed - Call Input Field
+     * @access public
+     * @param string $ShippingCarrierUsed 
+     */
+    public function ShippingCarrierUsed($ShippingCarrierUsed){
+        $this->input_fields['ShippingCarrierUsed'] = trim($ShippingCarrierUsed);
+    }
+    
+    /**
+     * ShippingTime - Call Input Field
+     * @access public
+     * @param dateTime $ShippingTime (YYYY-MM-DDTHH:MM:SS.SSSZ)
+     */
+    public function ShippingTime($ShippingTime){
+        $this->input_fields['ShippingTime'] = trim($ShippingTime);
+    }
+    
     
     
     
@@ -268,9 +454,9 @@ class Trading {
         
         $ErrorLanguage_string = '';
         
-        if(isset($this->standard_input_fields['ErrorLanguage'])){
+        if(isset($this->input_fields['ErrorLanguage'])){
             
-            $ErrorLanguage_string = '<ErrorLanguage>'.$this->standard_input_fields['ErrorLanguage'].'</ErrorLanguage>';            
+            $ErrorLanguage_string = '<ErrorLanguage>'.$this->input_fields['ErrorLanguage'].'</ErrorLanguage>';            
         }
         
         return $ErrorLanguage_string;
@@ -286,9 +472,9 @@ class Trading {
         
         $MessageID_string = '';
         
-        if(isset($this->standard_input_fields['MessageID'])){
+        if(isset($this->input_fields['MessageID'])){
             
-            $MessageID_string = '<MessageID>'.$this->standard_input_fields['MessageID'].'</MessageID>';
+            $MessageID_string = '<MessageID>'.$this->input_fields['MessageID'].'</MessageID>';
         }
         
         return $MessageID_string;
@@ -306,9 +492,9 @@ class Trading {
         // Prevent from running in production enviroment
         if($this->sandbox_mode){
             
-            if(isset($this->standard_input_fields['WarningLevel'])){
+            if(isset($this->input_fields['WarningLevel'])){
 
-                $WarningLevel_string = '<WarningLevel>'.$this->standard_input_fields['WarningLevel'].'</WarningLevel>';
+                $WarningLevel_string = '<WarningLevel>'.$this->input_fields['WarningLevel'].'</WarningLevel>';
             }
         }
         
@@ -324,9 +510,9 @@ class Trading {
         
         $DisputeExplanation_string = '';
         
-        if(isset($this->standard_input_fields['DisputeExplanation'])){
+        if(isset($this->input_fields['DisputeExplanation'])){
             
-            $DisputeExplanation_string = '<DisputeExplanation>'.$this->standard_input_fields['DisputeExplanation'].'</DisputeExplanation>';
+            $DisputeExplanation_string = '<DisputeExplanation>'.$this->input_fields['DisputeExplanation'].'</DisputeExplanation>';
         }
         
         return $DisputeExplanation_string;        
@@ -341,9 +527,9 @@ class Trading {
         
         $DisputeReason_string = '';
         
-        if(isset($this->standard_input_fields['DisputeReason'])){
+        if(isset($this->input_fields['DisputeReason'])){
             
-            $DisputeReason_string = '<DisputeReason>'.$this->standard_input_fields['DisputeReason'].'</DisputeReason>';
+            $DisputeReason_string = '<DisputeReason>'.$this->input_fields['DisputeReason'].'</DisputeReason>';
         }
         
         return $DisputeReason_string;
@@ -358,9 +544,9 @@ class Trading {
         
         $ItemID_string = '';
         
-        if(isset($this->standard_input_fields['ItemID'])){
+        if(isset($this->input_fields['ItemID'])){
             
-            $ItemID_string = '<ItemID>'.$this->standard_input_fields['ItemID'].'</ItemID>';
+            $ItemID_string = '<ItemID>'.$this->input_fields['ItemID'].'</ItemID>';
         }
         
         return $ItemID_string;        
@@ -375,9 +561,9 @@ class Trading {
         
         $OrderLineItemID_string = '';
         
-        if(isset($this->standard_input_fields['OrderLineItemID'])){
+        if(isset($this->input_fields['OrderLineItemID'])){
             
-            $OrderLineItemID_string = '<OrderLineItemID>'.$this->standard_input_fields['OrderLineItemID'].'</OrderLineItemID>';
+            $OrderLineItemID_string = '<OrderLineItemID>'.$this->input_fields['OrderLineItemID'].'</OrderLineItemID>';
         }
         
         return $OrderLineItemID_string;
@@ -392,14 +578,115 @@ class Trading {
         
         $TransactionID_string = '';
         
-        if(isset($this->standard_input_fields['TransactionID'])){
+        if(isset($this->input_fields['TransactionID'])){
             
-            $TransactionID_string = '<TransactionID>'.$this->standard_input_fields['TransactionID'].'</TransactionID>';
+            $TransactionID_string = '<TransactionID>'.$this->input_fields['TransactionID'].'</TransactionID>';
         }
         
         return $TransactionID_string;
     }
     
+    /**
+     * DisputeActivity XML
+     * @access private
+     * @return string 
+     */
+    private function _DisputeActivity(){
+        
+        $DisputeActivity_string = '';
+        
+        if(isset($this->input_fields['DisputeActivity'])){
+            
+            $DisputeActivity_string = '<DisputeActivity>'.$this->input_fields['DisputeActivity'].'</DisputeActivity>';
+        }
+        
+        return $DisputeActivity_string;
+    }
+    
+    /**
+     * DisputeID XML
+     * @access private
+     * @return string 
+     */
+    private function _DisputeID(){
+        
+        $DisputeID_string = '';
+        
+        if(isset($this->input_fields['DisputeID'])){
+            
+            $DisputeID_string = '<DisputeID>'.$this->input_fields['DisputeID'].'</DisputeID>';
+        }
+        
+        return $DisputeID_string;
+    }
+    
+    /**
+     * MessageText XML
+     * @access private
+     * @return string 
+     */
+    private function _MessageText(){
+        
+        $MessageText_string = '';
+        
+        if(isset($this->input_fields['MessageText'])){
+            
+            $MessageText_string = '<MessageText>'.$this->input_fields['MessageText'].'</MessageText>';
+        }
+        
+        return $MessageText_string;        
+    }
+    
+    /**
+     * ShipmentTrackNumber XML
+     * @access private
+     * @return string 
+     */
+    private function _ShipmentTrackNumber(){
+        
+        $ShipmentTrackNumber_string = '';
+        
+        if(isset($this->input_fields['ShipmentTrackNumber'])){
+            
+            $ShipmentTrackNumber_string = '<ShipmentTrackNumber>'.$this->input_fields['ShipmentTrackNumber'].'</ShipmentTrackNumber>';
+        }
+        
+        return $ShipmentTrackNumber_string;
+    }
+    
+    /**
+     * ShippingCarrierUsed XML
+     * @access private
+     * @return string 
+     */
+    private function _ShippingCarrierUsed(){
+        
+        $ShippingCarrierUsed = '';
+        
+        if(isset($this->input_fields['ShippingCarrierUsed'])){
+            
+            $ShippingCarrierUsed = '<ShippingCarrierUsed>'.$this->input_fields['ShippingCarrierUsed'].'</ShippingCarrierUsed>';
+        }
+        
+        return $ShippingCarrierUsed;
+    }
+    
+    /**
+     * ShippingTime XML
+     * @access private
+     * @return string 
+     */
+    private function _ShippingTime(){
+        
+        $ShippingTime_string = '';
+        
+        if(isset($this->input_fields['ShippingTime'])){
+            
+            $ShippingTime_string = '<ShippingTime>'.$this->input_fields['ShippingTime'].'</ShippingTime>';
+        }
+        
+        return $ShippingTime_string;
+    }
     
     
     
@@ -419,7 +706,7 @@ class Trading {
          
         if($value == ''){
             
-            $this->error['function'] = 'AddDispute';
+            $this->error['function'] = $this->call_reference;
             $this->error['message'] = 'The argument '.$argument.' is required.';
             return FALSE;
             
@@ -443,8 +730,8 @@ class Trading {
             'X-EBAY-API-CERT-NAME: '.$this->certificate_id,
             'X-EBAY-API-CALL-NAME: '.$this->call_reference,
             'X-EBAY-API-SITEID: '.$this->site_code['site_id'],
-            'Content-Type: "text/xml"',
-            'Content-Length: '.$this->content_length
+            //'Content-Type: "text/xml"',
+            //'Content-Length: '.$this->content_length
         );
         
     }
@@ -477,7 +764,7 @@ class Trading {
         // Build Headers
         $this->content_length = strlen($postData);
         $this->_build_headers();
-        
+               
         // Get URL
         $url = '';
         if($this->sandbox_mode){
@@ -485,7 +772,7 @@ class Trading {
         } else {
             $url = $this->gateway_uri['production'];
         }
-        
+
         // Setup
         $ch = curl_init();
 
@@ -495,13 +782,14 @@ class Trading {
         curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_HTTPHEADER, $this->headers);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
 
         // Execute
-        $response = curl_exec($ch);
-
+        $response = curl_exec($ch);       
+        
         // Report Errors
         if (curl_errno($ch) !== 0) {
-
+            
             $this->error['function'] = 'Send';
             $this->error['number'] = curl_errno($ch);
             $this->error['message'] = curl_error($ch);
@@ -513,7 +801,12 @@ class Trading {
         // Close
         curl_close($ch);
         $this->results = $response;
-        return TRUE;
+        
+        if($response == FALSE){
+            return FALSE;
+        } else {
+            return TRUE;
+        }
     }
     
 }
